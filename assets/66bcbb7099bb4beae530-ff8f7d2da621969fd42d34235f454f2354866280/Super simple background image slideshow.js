51b8854f34aac7024bab45d379ce1196a85c6a3c
Marcus Smith <marcussmith0711@gmail.com>
2017-04-25 14:58:28 -0500

    everything is working but the slide show

---
+++ assets/66bcbb7099bb4beae530-ff8f7d2da621969fd42d34235f454f2354866280/Super simple background image slideshow.js
@@ -0,0 +1,19 @@
+/*! slides | https://gist.github.com/mhulse/66bcbb7099bb4beae530 */
+(function($) {
+	
+	'use strict';
+	
+	var $slides = $('[data-slides]');
+	var images = $slides.data('slides');
+	var count = images.length;
+	var slideshow = function() {
+		$slides
+			.css('background-image', 'url("' + images[Math.floor(Math.random() * count)] + '")')
+			.show(0, function() {
+				setTimeout(slideshow, 5000);
+			});
+	};
+	
+	slideshow();
+	
+}(jQuery));
---
+++ assets/javascript/slideshow.js
@@ -0,0 +1,18 @@
+(function($) {
+	
+	'use strict';
+	
+	var $slides = $('[data-slides]');
+	var images = $slides.data('slides');
+	var count = images.length;
+	var slideshow = function() {
+		$slides
+			.css('background-image', 'url("' + images[Math.floor(Math.random() * count)] + '")')
+			.show(0, function() {
+				setTimeout(slideshow, 3000);
+			});
+	};
+	
+	slideshow();
+	
+}(jQuery));
