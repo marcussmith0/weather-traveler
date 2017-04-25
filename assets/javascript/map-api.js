bac51a35fc2df4f95b48ed5e365621f607a91f40
Steven Quast <smquast@gmail.com>
2017-04-25 18:17:32 -0500

    Added a map-api.js

--- assets/javascript/map-api.js
+++ assets/javascript/map-api.js
@@ -5,13 +5,38 @@
 // all the cities the path passes through, then give the city data to the weather API
 /////////////////////////////////////////////////////////////////////////////////////
 
+var oData = 
+{
+	origin : "",
+	destination : "",
+	date : ""
+};
+
 $(document).on("click", "#submit-btn", retrieveData);
 
-
+$(document).ready(displayData);
 
 function retrieveData()
 {
-	var sOrigin = $("#start-location").val().trim();
-	var sDestination = $(#destination).val().trim();
-	var sDate = $("#start-date").val().trim();
+	// console.log(Storage);
+	// sessionStorage.bob = 1;
+	// console.log("bob: " + sessionStorage.bob);
+	sessionStorage.origin = $("#start-location").val().trim();
+	sessionStorage.destination = $("#destination").val().trim();
+	sessionStorage.date = $("#start-date").val().trim();
+	window.location.href = "map.html";
+	
+	// displayData(oData);
+}
+
+function displayData()
+{
+	var mapElement = document.getElementById("map");
+	if(mapElement)
+	{
+		console.log(sessionStorage.origin);
+	}
+	// console.log(mapElement);
+	// console.log(sessionStorage);
+
 }
