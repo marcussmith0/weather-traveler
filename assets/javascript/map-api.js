c3f2a4d71190d731582a66d541d04116b2d6d0f3
Steven Quast <smquast@gmail.com>
2017-04-28 22:29:38 -0500

    streamlined API calls and response parsing for Google Maps API

--- assets/javascript/map-api.js
+++ assets/javascript/map-api.js
@@ -9,13 +9,12 @@ var oData =
 {
 	originLat : "",
 	originLong : "",
+	originLoc : "",
 	originTime : "",
 	destinationLat : "",
-	destinationLong : "", 
-	destinationTime : "",
-	originSet : false,
-	destinationSet : false,
-	timeSet : false
+	destinationLong : "",
+	destinationLoc : "", 
+	destinationTime : ""
 };
 
 var sGoogleMapsQueryURL = "http://maps.googleapis.com/maps/api/directions/json?";
@@ -28,9 +27,9 @@ $(document).ready(displayData);
 // Get Data and put it into Session Storage
 function retrieveData()
 {
-	sessionStorage.origin = escapeString($("#start-location").val().trim());
-	sessionStorage.destination = escapeString($("#destination").val().trim());
-	sessionStorage.date = escapeString($("#date").val().trim());
+	sessionStorage.origin = $("#start-location").val().trim();
+	sessionStorage.destination = $("#destination").val().trim();
+	sessionStorage.date = $("#date").val().trim();
 	window.location.href = "map.html";
 }
 
@@ -40,62 +39,7 @@ function displayData()
 	var mapElement = document.getElementById("map");
 	if(mapElement)
 	{
-		console.log("Origin: " + sessionStorage.origin);
-		console.log("Destination: " + sessionStorage.destination);
-		console.log("Date: " + sessionStorage.date);
-
 		var directionsService = new google.maps.DirectionsService;
-		var geocoder = new google.maps.Geocoder();
-
-		// Get Geocode Coordinates to pass to the Weather Map API
-
-		var latitude;
-		var longitude;
-
-		// make an API call to the geocoder for the origin
-		geocoder.geocode({'address': sessionStorage.origin}, function(results, status)
-			{
-				// if it worked, store data and check for completion
-				if (status == 'OK') 
-				{
-					var latLng = results[0].geometry.location;
-					oData.originLat = latLng.lat().toString();
-					oData.originLong = latLng.lng().toString();
-					oData.originSet = true;
-					checkStatus();
-      			} 
-
-      			// otherwise
-      			else 
-      			{
-        			$("#directions").append('<p class="error">Geocode was not successful for the following reason: ' + status + '</p>');
-      			}
-			});
-
-		// make an API call to the geocoder for the destination
-		geocoder.geocode({'address': sessionStorage.destination}, function(results, status)
-			{
-				// if it worked, store data and check for completion
-				if (status == 'OK') 
-				{
-					var latLng = results[0].geometry.location;
-					oData.destinationLat = latLng.lat().toString();
-					oData.destinationLong = latLng.lng().toString();
-					oData.destinationSet = true;
-					checkStatus();
-      			} 
-
-      			// otherwise
-      			else 
-      			{
-        			$("#directions").append('<p class="error">Geocode was not successful for the following reason: ' + status + '</p>');
-      			}
-			});
-
-		// console.log(latitude);
-		// console.log(longitude);
-		// console.log(directionsService);
-
 
         // Optionally create a map
         var directionsDisplay = new google.maps.DirectionsRenderer;
@@ -128,7 +72,7 @@ function displayData()
                 console.log(response);
 
                 // Display directions to the screen
-                var routes = response.routes;
+                var leg = response.routes[0].legs[0];
                 directionsDiv.html("<h2>Directions</h2>");
 
                 // Get current time
@@ -137,77 +81,54 @@ function displayData()
                 dateToPass = new Date(dateToPass.getTime() + nineHours);
                 if(dateToPass == "Invalid Date")
                 {
-                	console.log("invalid");
                 	dateToPass = new Date(Date.now());
                 }
                 console.log(dateToPass);
                 oData.originTime = dateToPass.valueOf() / 1000;
 
-                for(var routeIndex = 0; routeIndex < routes.length; routeIndex++)
+                // Add each leg to the time
+                dateToPass = new Date(dateToPass.getTime() + (leg.duration.value * 1000));
+
+                // get geographical data
+                oData.originLoc = leg.start_address;
+                oData.destinationLoc = leg.end_address;
+                oData.originLat = leg.start_location.lat();
+                oData.originLong = leg.start_location.lng();
+                oData.destinationLat = leg.end_location.lat();
+                oData.destinationLong = leg.end_location.lng();
+
+                // document the steps to take
+                var steps = leg.steps;
+                for(var stepIndex = 0; stepIndex < steps.length; stepIndex++)
                 {
-                	var legs = routes[routeIndex].legs;
-                	for(var legIndex = 0; legIndex < legs.length; legIndex++)
-                	{
-                		// Add each leg to the time
-                		dateToPass = new Date(dateToPass.getTime() + (legs[legIndex].duration.value * 1000));
-                		var steps = legs[legIndex].steps;
-                		for(var stepIndex = 0; stepIndex < steps.length; stepIndex++)
-                		{
-                			var direction = steps[stepIndex].instructions;
-                			var paragraph = $("<p>");
-                			paragraph.append(direction);
-                			directionsDiv.append(paragraph);
-                		}
-                	}
+                	var direction = steps[stepIndex].instructions;
+                	var paragraph = $("<p>");
+                	paragraph.append(direction);
+                	directionsDiv.append(paragraph);
                 }
-
+                	
+            	// calculate estimated time of arrival
                 oData.destinationTime = dateToPass.valueOf() / 1000;
-                oData.timeSet = true;
-                directionsDiv.append("<p><b>Total Travel Time</b></p><p>" + legs[0].duration.text + "</p>");
-                directionsDiv.append("<p><b>Estimated Time of Arrival:</b></p><p>" + dateToPass.toString() + "</p>");
-                console.log(dateToPass.toDateString());
-                console.log(dateToPass.toTimeString());
-                console.log(dateToPass.toString());
+                directionsDiv.append("<p><b>Total Travel Time</b></p><p>" + leg.duration.text + "</p>");
+                // directionsDiv.append("<p><b>Estimated Time of Arrival:</b></p><p>" + dateToPass.toString() + "</p>");
 
-                checkStatus();
+                passData();
             } 
 
             // otherwise
             else 
             {
                 directionsDiv.append('<p class="error">Directions request failed due to ' + status + "</p>");
+                directionsDiv.append('<p class="error">Please go back and try again.</p>');
             }
         });
 	}
 }
 
-// Make sure all data neccessary to make an API call to the OpenWeatherMap API is ready
-function checkStatus()
+// Pass all data neccessary to make an API call to the OpenWeatherMap API
+function passData()
 {
-	// if everything is ready
-	if(oData.originSet && oData.destinationSet && oData.timeSet)
-	{
-		console.log("We Got 'em");
-		// sending off origin data
-		// if (sessionStorage.date == )
-		deriveForecast(oData.originLat, oData.originLong);
-		deriveForecast(oData.destinationLat, oData.destinationLong, false, oData.destinationTime);
-
-	}
+	console.log(oData);
+	deriveForecast(oData.originLat, oData.originLong, oData.originLoc, false, oData.originTime);
+	deriveForecast(oData.destinationLat, oData.destinationLong, oData.destinationLoc, false, oData.destinationTime);
 }
-
-function escapeString(string)
-{
-	var sEscapedString = "";
-
-	for(var i = 0; i < string.length; i++)
-	{
-		if(string[i] !== " ")
-			sEscapedString += string[i];
-		else
-			sEscapedString += "+";
-	}
-
-	return sEscapedString;
-}
-
